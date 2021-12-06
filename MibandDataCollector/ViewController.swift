bda18fe333360b0e54b3bc8b6b0d60f0e4c86937
dflehel <44527037+dflehel@users.noreply.github.com>
2021-12-06 17:23:51 +0200

    Initial Commit

---
+++ MibandDataCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MibandDataCollector
+//
+//  Created by Lehel Dénes-Fazakas on 2021. 12. 06..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
